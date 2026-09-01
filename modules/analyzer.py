a75302205297adf72b1e36ad54a44178c350b5ab
mahreenahmed <45311681+mahreenahmed@users.noreply.github.com>
2026-09-01 15:03:43 +0800

    Update analyzer.py

--- modules/analyzer.py
+++ modules/analyzer.py
@@ -7,7 +7,7 @@ from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.retrievers import BM25Retriever, EnsembleRetriever
+from langchain_community.retrievers import BM25Retriever, EnsembleRetriever
 
 
 # ==========================================================
